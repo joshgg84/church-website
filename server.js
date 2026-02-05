5c40f3e544427e9336ab73e372b30530fff06778
JOSHGG <joshuagiwa440@gmail.com>
2026-02-05 23:06:04 +0100

    Server ready

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http')
+const fs = require('fs')
 const server = http.createServer((req, res) => {
-  res.end(`<h1 style="color: blue; font-size: 18px;">Welcome to Our Church!</h1>`)
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+    res.end(data)
   })
+});
   const port = process.env.PORT || 3000
   server.listen(port, '0.0.0.0', () => {
     console.log('Church website running')
