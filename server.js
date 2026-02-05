1a9be3822af19042436f2eb22c67fae13d76a543
JOSHGG <joshuagiwa440@gmail.com>
2026-02-05 22:28:42 +0100

    Testing server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-  res.end(`<h1>Welcome to Our Church!</h1>`)
+  res.end(`<h1 style="color: blue; font-size: 18px;">Welcome to Our Church!</h1>`)
   })
   const port = process.env.PORT || 3000
   server.listen(port, '0.0.0.0', () => {
