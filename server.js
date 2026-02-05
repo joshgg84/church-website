2bf2e88012d9026d519d5e72fc2d5ab8dbe12922
JOSHGG <joshuagiwa440@gmail.com>
2026-02-05 22:22:41 +0100

    Simple working server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-  res.end("Welcome to Our Church!")
+  res.end(`<h1>Welcome to Our Church!</h1>`)
   })
   const port = process.env.PORT || 3000
   server.listen(port, '0.0.0.0', () => {
